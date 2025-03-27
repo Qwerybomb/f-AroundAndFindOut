d34066d1add818c11773ada1af3a75b799b01dfc
isaac <isaac.j.mayo@gmail.com>
2025-03-26 19:56:52 -0700

    made code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scanTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scanTest.java
@@ -7,20 +7,28 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @TeleOp (name="AdvancedAutoBasketSpike", group="Robot")
 public class scanTest extends LinearOpMode {
-    private DistanceSensor DistanceSensor;
-    String distanceHolder = "";
+    private DistanceSensor distanceSensor;
+
+    public static String outputDistance(DistanceSensor Dist) {
+        String distanceHolder = "";
+        for (int i = 0; i == (Dist.getDistance(DistanceUnit.MM)); i++) {
+            distanceHolder = distanceHolder + "|";
+        }
+        return distanceHolder;
+    }
+
     public void runOpMode() {
-        DistanceSensor = hardwareMap.get(DistanceSensor.class, "distance_sensor");
+        distanceSensor = hardwareMap.get(DistanceSensor.class, "distance_sensor");
         telemetry.addData("Wai", "ting");
         telemetry.update();
         waitForStart();
+
+
+
+
+
         while (opModeIsActive()) {
-            distanceHolder = "";
-           for (int i = 0; i == (DistanceSensor.getDistance(DistanceUnit.MM)); i++) {
-               distanceHolder = distanceHolder + "|";
-           }
-           telemetry.addData("", distanceHolder);
-          telemetry.update();
+          telemetry.addData("", outputDistance(distanceSensor));
             }
     }
 
