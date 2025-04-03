488a5fbd694667c0a03c3441fd8e8e55d4ec805b
FTC10191 <ftc10191@gmail.com>
2025-04-03 18:28:10 -0500

    Improving replace method

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scanTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/scanTest.java
@@ -3,28 +3,41 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.UnaryOperator;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+import java.util.Arrays;
 
 @TeleOp (name="AppleVisionPro", group="Robot")
 public class scanTest extends LinearOpMode {
-    private DistanceSensor distanceSensor;
 
     public static String outputDistance(DistanceSensor Dist) {
-        String distanceHolder = "";
-        for (int i = 0; i == (Dist.getDistance(DistanceUnit.MM)); i++) {
-            distanceHolder = distanceHolder + "|";
+        StringBuilder distanceHolder = new StringBuilder();
+        for (int i = 0; i < (Dist.getDistance(DistanceUnit.CM)); i++) {
+            distanceHolder.append('|');
         }
-        return distanceHolder;
+        return distanceHolder.toString();
     }
+    public static String fastOutputDistance(DistanceSensor D) {
+        String Distance = String.valueOf(D.getDistance(DistanceUnit.CM));
+        List<String> list = Arrays.asList(Distance.split(""));
+        UnaryOperator<String> replaceMap = n -> {
+            return "|";};
+        list.replaceAll(replaceMap);
 
+    }
     public void runOpMode() {
-        distanceSensor = hardwareMap.get(DistanceSensor.class, "distance_sensor");
+        DistanceSensor distanceSensor = hardwareMap.get(DistanceSensor.class, "distance_sensor");
         telemetry.addData("Wai", "ting");
         telemetry.update();
         waitForStart();
 
         while (opModeIsActive()) {
-          telemetry.addData("", outputDistance(distanceSensor));
+          telemetry.addData("Dist", outputDistance(distanceSensor));
+          telemetry.addData("distanceInCM", distanceSensor.getDistance(DistanceUnit.CM));
+          telemetry.update();
             }
     }
 
